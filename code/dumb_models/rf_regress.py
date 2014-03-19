3001ce7e03858dfe06fb48569c4f35d553bed937
Ross Fadely <rossfadely@gmail.com>
2014-03-19 09:41:07 -0400

    typo

--- code/dumb_models/rf_regress.py
+++ code/dumb_models/rf_regress.py
@@ -22,7 +22,7 @@ def cv_loop(x, y, Nfolds=10, Nest=10):
     scores = np.zeros(Nfolds)
     for i in range(Nfolds):
         inds = np.random.perturbation(y.size)
-        Ntest = np.ciel(y.size / Nfolds)
+        Ntest = np.ceil(y.size / Nfolds)
         trninds = inds[Ntest:]
         tstinds = inds[:Ntest]
         xtest = x[tstinds]
